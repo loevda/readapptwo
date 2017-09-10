cb707c539b552c2714eb30f190868aaad6b95ca4
david2099 <loevenbruck.david@gmail.com>
2017-09-10 23:36:28 +0200

    refactor: Added api in utils

---
+++ src/utils/api.js
@@ -0,0 +1,22 @@
+/**
+ * Created by david2099 on 10/09/17.
+ */
+const api = 'http://localhost:5001'
+
+let token = localStorage.token
+if (!token)
+    token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+    'Accept': 'application/json',
+    'Authorization': token
+}
+
+export function fetchCategoryPosts (categoryId) {
+    return fetch(`${api}/${categoryId}/posts`, { headers })
+        .then(res => res.json())
+        .then(data => data.filter((post) => {
+            return !post.deleted
+        }))
+}
+
